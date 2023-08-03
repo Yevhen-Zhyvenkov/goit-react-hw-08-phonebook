d5f073ef9ad44cfd4341f01f6498617008d454d5
Yevhen-Zhyvenkov <116755275+Yevhen-Zhyvenkov@users.noreply.github.com>
2023-08-03 19:53:54 +0000

    Deploying to gh-pages from @ Yevhen-Zhyvenkov/goit-react-hw-08-phonebook@198430cfddedd437beed575dbb73da172792a5e7 🚀

---
+++ static/js/151.0f40be2f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[151],{3151:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var o="Home_container__AHomM",c="Home_title__qAdt+",r=n(184);function a(){return(0,r.jsx)("div",{className:o,children:(0,r.jsx)("h1",{className:c,children:"Welcome phonebook"})})}}}]);
+//# sourceMappingURL=151.0f40be2f.chunk.js.map
